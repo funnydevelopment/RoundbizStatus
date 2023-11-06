544c71cdca5be90254da6bfae7526a3779f48f65
funnydevelopment <ya.giggs2013@yandex.ru>
2023-11-06 03:29:09 +0300

    updated texts

---
+++ core/message_texts.py
@@ -0,0 +1,11 @@
+HELLO_TEXT = (
+    "Hello, {user_name}!\n\nI am your Telegram bot. To check a request, send me a UUID "
+    "(Universally Unique Identifier), and I will verify it for you. Make sure the UUID "
+    "is correctly formatted."
+)
+
+WRONG_USER_TEXT = (
+    "Access to the Telegram bot is currently unavailable. Please request access."
+)
+
+WRONG_FORMAT_TEXT = "The text that was sent is not UUID format."
